91711ca1de1425c8dfcee2d4a549037f063a4935
xkid-discord <81926491+xkid-discord@users.noreply.github.com>
2021-04-04 17:44:18 -0500

    Update kohls-admin.lua

--- kohls-admin.lua
+++ kohls-admin.lua
@@ -1,7 +1,8 @@
 local person = game.Players.LocalPlayer
 local someonebanned = false
 local someonelogspam = false
-
+local someonepunished = false
+local oofgp = game:GetService("Players")
 person.Chatted:Connect(function(chat)
 local args = string.split(chat, " ")
 local command = args[1]
@@ -38,6 +39,58 @@ if command == "x!load" then
 loadstring(game:HttpGet('https://pastebin.com/raw/9SPMjxq2', true))()
 end
 
+if command == "x!shutdown" then
+
+while wait() do
+game:GetService("Players"):Chat(tostring("h Shutting down..."))
+game:GetService("Players"):Chat("swagify all all all all all all all")
+
+oofgp:Chat("gear me 00000000000000000094794847")
+wait(.5)
+oofgp.LocalPlayer.Character.Humanoid:EquipTool(oofgp.LocalPlayer.Backpack.VampireVanquisher)
+wait()
+oofgp:Chat("size me .3")
+oofgp:Chat("size me .3")
+oofgp:Chat("size me .3")
+oofgp:Chat("size me .3")
+oofgp:Chat("size me .3")
+oofgp:Chat("size me .3")
+oofgp:Chat("size me .3")
+oofgp:Chat("size me .3")
+oofgp:Chat("size me .3")
+oofgp:Chat("size me .3")
+oofgp:Chat("size all nan")
+end
+
+end
+
+
+
+
+if command == "x!autopunish" then
+
+ game.StarterGui:SetCore("SendNotification", {
+        Title = "Automatically punishing ".. name,
+        Text = "Lmao"
+})
+
+someonepunished = true
+
+while wait(1) do
+if someonepunished == false then break end
+game:GetService("Players"):Chat(tostring("punish ".. name))
+end
+
+end
+
+if command == "x!unautopunish" then
+    someonepunished = false
+ game.StarterGui:SetCore("SendNotification", {
+        Title = "Stopped punishing ".. name,
+        Text = "Lmao"
+})
+game:GetService("Players"):Chat(tostring("unpunish ".. name))
+end
 -- UNBAN CMD UNBAN CMD UNBAN CMD UNBAN CMD UNBAN CMD UNBAN CMD UNBAN CMD UNBAN CMD UNBAN CMD 
 
 
