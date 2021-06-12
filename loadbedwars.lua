8881e6b984486f36ad9942f4483155f07b370a10
xkid-discord <81926491+xkid-discord@users.noreply.github.com>
2021-06-12 11:09:28 -0500

    Create loadbedwars.lua

---
+++ loadbedwars.lua
@@ -0,0 +1,40 @@
+wait(5)
+local mt = getrawmetatable(game)
+local old = mt.__namecall
+local protect = newcclosure or protect_function
+
+setreadonly(mt, false)
+mt.__namecall = protect(function(self, ...)
+	local method = getnamecallmethod()
+	if method == "Kick" then
+		wait(9e9)
+		return
+	end
+	return old(self, ...)
+end)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DiamondHands-Exploit/BedWarsExploit/main/Source.lua"))()
+
+ game.StarterGui:SetCore("SendNotification", {
+        Title = "Loaded!",
+        Text = "Sucessfully loaded lego haxx!"
+})
+
+while wait() do
+   pcall(function()
+       for i,v in pairs(game:GetService("Workspace").Map.Blocks:GetChildren()) do
+           if v.Name == "bed" and v.Covers.BrickColor ~= game.Players.LocalPlayer.TeamColor and game.Players.LocalPlayer:DistanceFromCharacter(v.Position) < 150 then
+               spawn(function()
+                   game:GetService("ReplicatedStorage").rbxts_include.node_modules.net.out._NetManaged.BreakBlock:InvokeServer({
+                       ["blockRef"] = {
+                           ["blockPosition"] = v.Position/3--.new(186, 14, 108)
+                       },
+                       ["hitPosition"] = v.Position,
+                       ["hitNormal"] = Vector3.new(1, 0, 0)
+                   })
+               end)
+           end
+       end
+   end)
+end
+
